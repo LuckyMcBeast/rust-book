ce442332fcd7bbf629cdf0540d896e9bb07b655c
Cullen McClellan <cullen.mcclellan@mcullenm.dev>
2023-03-13 02:12:35 -0400

    Completes suggested fibonacci calculation program

---
+++ fibonacci/src/main.rs
@@ -0,0 +1,21 @@
+const GOLDEN_RATIO: f64 = 1.618034;
+
+fn main() {
+    println!("{}", get_fibonacci(6));
+}
+
+fn get_fibonacci(nth: u32) -> u32 {
+    div_by_sqrt_of_5(gr_to_pow_of(nth as i32) - one_minus_gr_to_pow_of(nth as i32))
+}
+
+fn gr_to_pow_of(nth: i32) -> f64 {
+    GOLDEN_RATIO.powi(nth)
+}
+
+fn one_minus_gr_to_pow_of(nth: i32) -> f64 {
+    (1.0 - GOLDEN_RATIO).powi(nth)
+}
+
+fn div_by_sqrt_of_5(num: f64) -> u32 {
+    (num / (5 as f64).sqrt()).round() as u32
+}
